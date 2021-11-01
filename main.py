f7f208a859d74f8947597f95c032028a3c2d75ee
Étienne Machabée <mace2801@usherbrooke.ca>
2021-11-01 19:29:47 -0400

    Ajout du support du virage. Reste à essayer dans blender.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     try:
         vehicule = v.vehiculesim(30, [0, 0, 0], 0)
         vehicule.speed(0.18)
-        vehicule.heading(0)
-        vehicule.backward()
+        vehicule.turn_straight()
+        vehicule.forward()
         for i in range(30): # Pour 90 frames (3 secondes)
             vehicule.update()
             time.sleep(1/30)
@@ -15,5 +15,8 @@ if __name__ == "__main__":
         for i in range(30): # Pour 90 frames (3 secondes)
             vehicule.update()
             time.sleep(1/30)
+        vehicule.turn(45)
+        vehicule.plot()
+
     except:
         print("Échec")
--- vehiculesim.py
+++ vehiculesim.py
@@ -2,7 +2,6 @@
 # Simulation dans blender
 
 import math
-import _thread
 
 class vehiculesim:
 
@@ -10,6 +9,7 @@ class vehiculesim:
     _framelength = 0 # Longueur d'un frame, donc le t dans les calculs
     _heading = 0 # Cap du véhicule en radians
     _vitesse = 0 # Vitesse du véhicule actuelle en m/s
+    _angleroues = math.pi/2 # Angle des roues, 90 degrés est le centre
     _acceleration = 0 # Accélération du véhicule actuelle, en m/s2
     _state = 0 # État, 0 = stop, 1 = avancer, 2 = arrêté
 
@@ -24,7 +24,7 @@ class vehiculesim:
         pourcentage = vitesse/5 # TODO changer la vitesse maximale, ce qui devrait nous donner la bonne valeur
         return pourcentage
 
-    # Converti l'angle des roues en rayon de virage en mètres
+    # Converti l'angle des roues en radians en rayon de virage en mètres
     # TODO utiliser les vraies valeurs
     def anglearayon(self, angle):
         rayon = 0.2 # mètres
@@ -33,18 +33,21 @@ class vehiculesim:
     def speed(self, vitesse):
         self._vitesse = vitesse
 
-    def heading(self, heading):
-        self._heading = math.radians(heading)
+    # Mettre les roues droites
+    def turn_straight(self):
+        self._angleroues = math.radians(90-90)
+
+    # Tourner à un angle, en degrés. 90 est tout droit.
+    def turn(self, angle):
+        self._angleroues = math.radians(90-angle)
 
     # Avancer
     def forward(self):
         self._state = 1
 
-
     # Reculer
     def backward(self):
         self._state = 2
-
 
     # Arrêter
     def stop(self):
@@ -53,6 +56,16 @@ class vehiculesim:
 
     # Méthode à rouler à chaque frame qui va automatiquement déplacer le véhicule. Ça sert à simuler le comportement du vrai véhicule
     def update(self):
+        # Calcul du cap
+        circon = self.anglearayon(self._angleroues) * 2 * math.pi
+        if self._state == 1:
+            distanceframe = self._vitesse * self._framelength
+            self._heading = self._heading + ((distanceframe/circon) * 2 * math.pi)
+        elif self._state == 2:
+            distanceframe = -1 * self._vitesse * self._framelength
+            self._heading = self._heading - ((distanceframe/circon) * 2 * math.pi)
+
+        # Déplacement
         if self._state == 1:
             self._coordinates[0] = self._coordinates[0] + (self._vitesse * self._framelength * math.cos(self._heading))
             self._coordinates[1] = self._coordinates[1] + (self._vitesse * self._framelength * math.sin(self._heading))
