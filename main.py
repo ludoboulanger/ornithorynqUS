0f37794da1a0179986f793020fc37d36e2cad31b
Étienne Machabée <mace2801@usherbrooke.ca>
2021-11-01 18:51:35 -0400

    Ajout de la méthode pour déplacer le véhicule en fonction du cap. Vitesse constante pour l'instant. Pas d'accélération encore.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import vehiculesim as v
+import time
+import _thread
 
 if __name__ == "__main__":
+    try:
+        vehicule = v.vehiculesim(30, [0, 0, 0], 0)
+        vehicule.speed(0.18)
+        vehicule.heading(0)
+        vehicule.backward()
+        for i in range(30): # Pour 90 frames (3 secondes)
+            vehicule.update()
+            time.sleep(1/30)
+        vehicule.stop()
+        for i in range(30): # Pour 90 frames (3 secondes)
+            vehicule.update()
+            time.sleep(1/30)
+    except:
+        print("Échec")
