2a2321412b6ad6d9bfb50315a0e4b581d94eaee5
ludoboulanger <ludo.boulanger@gmail.com>
2021-11-04 11:43:36 -0400

    Move trajectories to own directory

---
+++ vehicle/vehicle_blend.py
@@ -0,0 +1,15 @@
+import bpy
+from bpy import data as D
+import numpy as np
+
+
+class Vehicle:
+    
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+        self.vel = 
+        
+        
