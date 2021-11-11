86bdc2fb4a637fd20365103e78b2dcbe6f18ff07
Jonathan <degj2706@usherbrooke.ca>
2021-11-11 10:58:23 -0500

    some modifs were not linked correctly

--- capteur_distance/capteur_distance.py
+++ capteur_distance/capteur_distance.py
@@ -20,6 +20,7 @@ class DistanceSensor:
     def get_raw_distance(self, debug=False):
         bpy.context.view_layer.update()
         closest_obstacle_distance = None
+        closest_obstacle_name = None
         self.location = self.sensor.matrix_world.translation
         vehicule_location = self.vehicule.matrix_world.translation
         self.vector = self.location - vehicule_location
@@ -36,6 +37,7 @@ class DistanceSensor:
                     obstacle_distance = (obj.location - self.location).length * 1000
                     if closest_obstacle_distance is None or obstacle_distance < closest_obstacle_distance :
                         closest_obstacle_distance = obstacle_distance
+                        closest_obstacle_name = obj.name
                     if debug : 
                         in_range.append({
                             "name" : obj.name,
@@ -61,31 +63,31 @@ class DistanceSensor:
                 print(str(obj))
             print("----------------------------------------------------------")
                 
-        return closest_obstacle_distance
+        return closest_obstacle_distance, closest_obstacle_name
     
     
     def get_sim_distance(self, debug=False):
-        distance = self.get_raw_distance(debug=debug)
+        distance, name = self.get_raw_distance(debug=debug)
         if distance is None or distance <= 0:
             return distance
-        return self.apply_trendline(raw_distance=distance)
+        return self.apply_trendline(raw_distance=distance), name
     
-    
+    #To do - change trendline values
     def apply_trendline(self,raw_distance):
         return 0.9207 * raw_distance - 34.295
 
 
 def sim_raw_single(sensor):
     print("\n ----------------------- Single object simulation - Raw distance calculated ----------------------- \n")
-    obstacle_distance = sensor.get_raw_distance(debug=True)
-    print_obstacle_distance(distance=obstacle_distance)
+    obstacle_distance, name = sensor.get_raw_distance(debug=True)
+    print_obstacle_distance(distance=obstacle_distance, name=name)
     print("\n -------------------------------------------------------------------------------------------------- \n")
 
 
 def sim_corrected_single(sensor):
     print("\n ----------------------- Single object simulation - distance calculated with trendline ----------------------- \n")
-    obstacle_distance = sensor.get_sim_distance(debug=True)
-    print_obstacle_distance(distance=obstacle_distance)
+    obstacle_distance, name = sensor.get_sim_distance(debug=True)
+    print_obstacle_distance(distance=obstacle_distance, name=name)
     print("\n ------------------------------------------------------------------------------------------------------------- \n")
       
         
@@ -94,17 +96,29 @@ def sim_corrected_multiple(sensor, start_pos, start_angle, final_angle, angle_in
     sensor.vehicule.location = start_pos
     for angle in range(start_angle, final_angle + angle_incr, angle_incr):
         sensor.vehicule.rotation_euler = (0,0, radians(angle))
-        obstacle_distance = sensor.get_sim_distance()
+        obstacle_distance, name = sensor.get_sim_distance()
         print("\nCurrent vehicule angle around z axis : " + str(angle))
-        print_obstacle_distance(distance=obstacle_distance)
+        print_obstacle_distance(distance=obstacle_distance, name=name)
+    print("\n ------------------------------------------------------------------------------------------------------------- \n")
+ 
+ 
+def sim_raw_multiple(sensor, start_pos, start_angle, final_angle, angle_incr):
+    print("\n ----------------------- Multiple object simulation - distance calculated with trendline ----------------------- \n")
+    sensor.vehicule.location = start_pos
+    for angle in range(start_angle, final_angle + angle_incr, angle_incr):
+        sensor.vehicule.rotation_euler = (0,0, radians(angle))
+        obstacle_distance, name = sensor.get_raw_distance(debug=True)
+        print("\nCurrent vehicule angle around z axis : " + str(angle))
+        print_obstacle_distance(distance=obstacle_distance, name=name)
     print("\n ------------------------------------------------------------------------------------------------------------- \n")
   
+         
         
-def print_obstacle_distance(distance):
+def print_obstacle_distance(distance, name):
     if distance is None or distance <= 0: 
         print("No obstacle found")
     else :
-        print("Obstacle found at distance : " + str(distance) + " mm")
+        print(name + " found at distance : " + str(distance) + " mm")
 
 
 def main():
@@ -116,11 +130,15 @@ def main():
     # Test a single obstacle - raw distance calculated
     sim_raw_single(sensor)
     
+    # Test multiple objects - raw distance calculated
+    sim_raw_multiple(sensor, start_pos=(0,0,0), start_angle=0, final_angle=360, angle_incr=10)
+    
+    
     # Test a single obstacle - corrected distance calculated using experimental trendline
-    sim_corrected_single(sensor)
+    #sim_corrected_single(sensor)
     
     # Test multiple objects - corrected distance calculated using experimental trendline
-    sim_corrected_multiple(sensor, start_pos=(0,0,0), start_angle=0, final_angle=360, angle_incr=10)
+    #sim_corrected_multiple(sensor, start_pos=(0,0,0), start_angle=0, final_angle=360, angle_incr=10)
 
 
 if __name__ == "__main__":
