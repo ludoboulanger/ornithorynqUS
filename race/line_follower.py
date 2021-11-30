7af357a73299e36033683831c67d29975386516f
Victor <vicfreve@gmail.com>
2021-11-30 11:51:48 -0500

    Line follower v1

---
+++ race/line_follower.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+from Line_Follower_Lib import Line_Follower
+import time
+
+class LineFollower():
+
+    def __init__(self):
+        self.last_angle = 0
+        self.lf = Line_Follower()
+        self.lf.references = np.load("references_lf.npy")
+
+    def lf_read_digital(self):
+        return self.lf.read_digital()
+    
+    def is_over_line(self):
+        if np.any(self.lf_read_digital()):
+            return True
+        else:
+            return False
+    
+    def is_race_over(self):
+        return np.count_nonzero(self.lf_read_digital)>=3
+
+    def get_angle_to_turn(self):
+        read = self.lf_read_digital()
+        print(read)
+
+        if read == [0, 0, 0, 0, 0]:
+            angle = self.last_angle
+        else:
+            angle = (2 - np.mean(np.nonzero(read))) * 90/3
+        self.last_angle = angle
+        print("Angle: ", angle)
+        return math.radians(angle)
+    
+    def calibrate(self, vehicle):
+        references = [0, 0, 0, 0, 0]
+        print("cali for module:\n  first put all sensors on white, then put all sensors on black")
+        mount = 100
+        vehicle.turn(70)
+        print("\n cali white")
+        time.sleep(4)
+        vehicle.turn(90)
+        white_references = self.lf.get_average(mount)
+        vehicle.turn(95)
+        time.sleep(0.5)
+        vehicle.turn(85)
+        time.sleep(0.5)
+        vehicle.turn(90)
+        time.sleep(1)
+
+        vehicle.turn(110)
+        print("\n cali black")
+        time.sleep(4)
+        vehicle.turn(90)
+        black_references = self.lf.get_average(mount)
+        vehicle.turn(95)
+        time.sleep(0.5)
+        vehicle.turn(85)
+        time.sleep(0.5)
+        vehicle.turn(90)
+        time.sleep(1)
+
+        for i in range(0, 5):
+            references[i] = (white_references[i] + black_references[i]) / 2
+        self.lf.references = references
+        print("Middle references =", references)
+        time.sleep(1)
+        np.save("references_lf.npy", references)
+
--- race/main.py
+++ race/main.py
@@ -1,25 +1,43 @@
 import time
 from distance_sensor import DistanceSensor
+from line_follower import LineFollower
+from vehicle import vehicle
+from math import degrees
 
 
-def race(timeout, log=False):
+def race(timeout, log=False, calibrate = False):
+    vehicle = vehicle()
+    vehicle.forward()
+    vehicle.turn_straight()
+    vehicle.speed(20)
+    line_follower = LineFollower()
     start_time = time.time()
     time_elapsed = 0
     is_race_over = False  # we should somehow get the information when we find the finish line (T shape)
     distance_sensor = DistanceSensor(channel=20, log=log)
+
+
+    if(calibrate):
+        line_follower.calibrate(vehicle)
+
     while time_elapsed < timeout and not is_race_over:
         time_elapsed = time.time() - start_time
         closest_obstacle_distance = distance_sensor.get_corrected_distance()
-        is_race_over = distance_sensor.should_avoid(closest_obstacle_distance)  # for testing purposes
+        is_race_over =   line_follower.is_race_over()
         if log:
             print(f'closest obstacle distance : {closest_obstacle_distance}')
             print(f'Car racing, time elapsed : {time_elapsed}')
+        angle = line_follower.get_angle_to_turn()
+        vehicle.turn(degrees(angle)+90)
+    vehicle.speed(0)
     return time_elapsed
 
 
 if __name__ == '__main__':
+    calibrate = True
+        
     log_main = True
     race_timeout = 10  # seconds
-    race_time = race(timeout=race_timeout, log=log_main)
+    race_time = race(timeout=race_timeout, log=log_main, calibrate=calibrate)
     if log_main:
         print(f'Race over, time elapsed : {race_time}, timeout exceeded? : {race_time > race_timeout}')
