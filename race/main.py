d442c30ec399903c2146fddf07f6f7e56757b421
Jonathan <degj2706@usherbrooke.ca>
2021-12-01 11:05:05 -0500

    use etiennes code for vehicle instead of backwheels

--- race/main.py
+++ race/main.py
@@ -2,7 +2,6 @@ import time
 from distance_sensor import DistanceSensor
 from line_follower import LineFollower
 from picar import vehicle
-from picar import back_wheels
 from math import degrees
 from enum import Enum
 
@@ -15,11 +14,10 @@ class States(Enum):
 
 def race(timeout, log=False, calibrate=False):
     # Initialize car and sensors
-    bw = back_wheels.Back_Wheels()
     v = vehicle.Vehicle()
-    bw.forward()
+    v.forward()
     v.turn_straight()
-    bw.speed(0, 0)
+    v.speed(0)
     line_follower = LineFollower()
     distance_sensor = DistanceSensor(channel=20, log=log)
 
@@ -31,14 +29,14 @@ def race(timeout, log=False, calibrate=False):
     if calibrate:
         line_follower.calibrate(v)
     input("Ready...set...")
-    bw.speed(80, 80)
+    v.speed(80)
     start_time = time.time()
     while time_elapsed < timeout and not is_race_over:
         time_elapsed = time.time() - start_time
         closest_obstacle_distance = distance_sensor.get_corrected_distance()
         if distance_sensor.should_avoid(closest_obstacle_distance):
             current_state = States.AVOID
-            bw.speed(0, 0)  # instead of stopping, we should avoid obstacle
+            v.stop()  # instead of stopping, we should avoid obstacle
             # call avoid function here or while loop ?
             break  # remove when avoid is implemented
         is_race_over = line_follower.is_race_over()
@@ -48,9 +46,9 @@ def race(timeout, log=False, calibrate=False):
         # ce code ne ne semble plus bien fonctionner, a voir pourquoi
         desired_angle = line_follower.get_angle_to_turn()
         diff = desired_angle - v._wheel_angle
-        v.turn(90 - degrees(v._wheel_angle + diff * 0.5))
+        v.turn(degrees(v._wheel_angle + diff * 0.5)) #a confirmer si ca tourne comme faut
     current_state = States.DONE
-    bw.speed(0, 0)
+    v.stop()
     return time_elapsed
 
 if __name__ == '__main__':
