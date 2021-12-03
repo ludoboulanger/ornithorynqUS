1b90bfa592988d70f6660d0ff79cde510a2f4fd0
Jonathan <degj2706@usherbrooke.ca>
2021-12-03 10:51:22 -0500

    debugging

--- race/main.py
+++ race/main.py
@@ -6,6 +6,7 @@ from math import degrees
 import math
 from enum import Enum
 import asyncio
+import RPi.GPIO as GPIO
 
 
 class States(Enum):
@@ -29,14 +30,13 @@ WAIT_TIME = 5
 
 
 
-def race(timeout, log=False, calibrate=False):
+def race(v, timeout, log=False, calibrate=False):
     # Initialize car and sensors
-    v = vehicle.Vehicle()
     v.forward()
     v.turn_straight()
     v.stop()
     line_follower = LineFollower()
-    distance_sensor = DistanceSensor(channel=5, avoid_distance=AVOID_DISTANCE, log=log)
+    distance_sensor = DistanceSensor(channel=16, avoid_distance=AVOID_DISTANCE, log=log)
 
     # Init variables
     time_elapsed = 0
@@ -53,6 +53,7 @@ def race(timeout, log=False, calibrate=False):
         # count+=1
         time_elapsed = time.time() - start_time
         if(current_state == States.FOLLOW_LINE):
+            print(f"------------------Current state : {current_state}---------------")
             closest_obstacle_distance = distance_sensor.get_corrected_distance()
             if closest_obstacle_distance > 0:
                 if distance_sensor.should_slow_down(closest_obstacle_distance):
@@ -74,16 +75,19 @@ def race(timeout, log=False, calibrate=False):
             v.turn(current_angle + diff * 0.5)
 
         elif(current_state == States.OBSTACLE_WAITING):
+            print(f"------------------Current state : {current_state}---------------")
             v.stop()
             time.sleep(WAIT_TIME)
             current_state = States.OBSTACLE_BACKWARD
         elif(current_state == States.OBSTACLE_BACKWARD):
+            print(f"------------------Current state : {current_state}---------------")
             v.backward()
             v.turn_straight()
             v.speed(50)
             if closest_obstacle_distance >= BACKWARD_DISTANCE:
                 current_state = States.OBSTACLE_TURN
         elif(current_state == States.OBSTACLE_TURN):
+            print(f"------------------Current state : {current_state}---------------")
             time_in_turn = math.sqrt(0.3**2+(DISTANCE_WHEELS/2)**2)/(v._speed)        
             v.forward()
             v.speed(TURN_SPEED)   
@@ -91,6 +95,7 @@ def race(timeout, log=False, calibrate=False):
             time.sleep(time_in_turn)
             current_state = States.OBSTACLE_FIND_LINE
         elif(current_state == States.OBSTACLE_FIND_LINE):
+            print(f"------------------Current state : {current_state}---------------")
             v.turn(90-FIND_LINE_ANGLE)
             if line_follower.is_over_line():
                 current_state= States.FOLLOW_LINE    
@@ -100,9 +105,16 @@ def race(timeout, log=False, calibrate=False):
 
 
 if __name__ == '__main__':
-    calibrate = False
-    log_main = True
-    race_timeout = 80  # seconds
-    race_time = race(timeout=race_timeout, log=log_main, calibrate=calibrate)
-    if log_main:
-        print(f'Race over, time elapsed : {race_time}, timeout exceeded? : {race_time > race_timeout}')
+    v = vehicle.Vehicle()
+    try: 
+        calibrate = False
+        log_main = True
+        race_timeout = 80  # seconds
+        race_time = race(v, timeout=race_timeout, log=log_main, calibrate=calibrate)
+        GPIO.cleanup()
+        if log_main:
+            print(f'Race over, time elapsed : {race_time}, timeout exceeded? : {race_time > race_timeout}')
+    except:
+        v.stop()
+        GPIO.cleanup()
+        
